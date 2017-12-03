103e0c7695ec3c812a05b65e3959a39de652d2f4
Neha Yadav <nehay100@gmail.com>
2017-12-03 15:21:31 -0800

    Updated master files with tabs and added comparision tab code

--- server.R
+++ server.R
@@ -1,26 +1,41 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
+# Master File
+library(dplyr)
+library(ggplot2)
 library(shiny)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
-  })
+  # Home Tab
   
+  # Map Tab
+  
+  # Car Insurance Tab
+  
+  # Comparision Tab
+      dataPlot <- read.csv(file = 'data/bad-drivers.csv', stringsAsFactors = FALSE)
+      
+      # Change column names of data frame
+      colnames(dataPlot) <- c("State", "Number.Collisions", "Collisions.Speeding", "Collisions.Alcohol", "Collisions.Not.Distracted", "Collisions.No.Previous", "Car.Insurance", "Losses.By.Insurance")
+      
+      # Compute US average of fatal collisions per billion miles
+      usMeanDataFrame <- summarize(dataPlot, mean = mean(Number.Collisions))
+      usMean <- usMeanDataFrame[1,1]
+      
+      output$higherAverage <- renderPlot({
+        # filter out data frame for states higher than US mean
+        higherStates <- filter(dataPlot, Number.Collisions > usMean)
+        
+        # draw the histogram with the specified number of bins
+        ggplot(higherStates) + geom_point(aes_string(x = "State", y = input$higherY, color = input$higherColorType)) + ggtitle("Analysis of States Data Higher than US Mean")
+      })
+      
+      output$lowerAverage <- renderPlot({
+        # filter out data frame for states lower than US mean
+        lowerStates <- filter(dataPlot, Number.Collisions < usMean)
+        
+        # draw the histogram with the specified number of bins
+        ggplot(lowerStates) + geom_point(aes_string(x = "State", y = input$lowerY, color = input$lowerColorType)) + ggtitle("Analysis of States Data Lower than US Mean")
+      })
+  # Alcohol Comparision Tab
+      
 })
--- ui.R
+++ ui.R
@@ -1,33 +1,55 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
+# Master File
 library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
-    
     # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("distPlot")
+      navbarPage("INFO 201 Final Project",
+                 #tags$style(type = 'text/css', '.navbar {
+                           #font-family: Arial;
+                           # font-size: 20px;}'),    
+                 tabPanel("Home"),
+                 tabPanel("Map"),
+                 tabPanel("Car Insurance Premiums"),
+                 tabPanel("Comparision", 
+                          # Application title
+                          titlePanel("Comparision of Data with US Average"),
+                          
+                          # Sidebar with a slider input for number of bins 
+                          sidebarLayout(
+                            sidebarPanel(
+                              # Graphs for states larger than US State average
+                              # user inserts which variable represents x axis
+                              radioButtons("higherY", "Choose what variable is on Y axis for Higher States Data: ",
+                                           choices = list("Speeding" = 'Collisions.Speeding', "Alcohol" = 'Collisions.Alcohol', "Not Distracted" = 'Collisions.Not.Distracted'),
+                                           selected = 'Collisions.Speeding'),
+                              
+                              # user inserts which variable will be represented by color
+                              radioButtons("higherColorType", "Choose which variable to display by color for Higher States Data: ",
+                                           choices = list("Car Insurance" = 'Car.Insurance', "Losses on Insurance Companies" = 'Losses.By.Insurance'),
+                                           selected = 'Car.Insurance'),
+                              
+                              # Graphs for states lower than US State average
+                              # user inserts which variable represents x axis
+                              radioButtons("lowerY", "Choose what variable is on Y axis for Lower States Data: ",
+                                           choices = list("Speeding" = 'Collisions.Speeding', "Alcohol" = 'Collisions.Alcohol', "Not Distracted" = 'Collisions.Not.Distracted'),
+                                           selected = 'Collisions.Speeding'),
+                              
+                              # user inserts which variable will be represented by color
+                              radioButtons("lowerColorType", "Choose which variable to display by color for Lower States Data: ",
+                                           choices = list("Car Insurance" = 'Car.Insurance', "Losses on Insurance Companies" = 'Losses.By.Insurance'),
+                                           selected = 'Car.Insurance')
+                            ),
+                            
+                            # Show a plot of the generated distribution
+                            mainPanel(
+                              plotOutput("higherAverage"),
+                              plotOutput("lowerAverage")
+                            )
+                          )),
+                 tabPanel("Alcohol Comparision")
+      )
     )
   )
-))
+)
